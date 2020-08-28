fbe5817091e0a55fc0dcc046207fbfde2eda92ba
Tomislav Valentić <tvalentic@combis.hr>
2020-08-28 12:39:46 +0200

    Finished 71 lesson

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.components";
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route path="/" component={HomePage} />
+      </Switch>
     </div>
   );
 }
