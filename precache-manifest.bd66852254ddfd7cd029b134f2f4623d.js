6e9899301c4f9179da25e5d8dc75cefa5c4a5a43
Tomislav Valentić <tvalentic@combis.hr>
2020-08-28 11:14:04 +0200

    Updates

---
+++ precache-manifest.bd66852254ddfd7cd029b134f2f4623d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afe2dd90e4d260fd8dd5006bfb4ce499",
+    "url": "/crwn-clothing/index.html"
+  },
+  {
+    "revision": "ba7c54e9399d5c233d37",
+    "url": "/crwn-clothing/static/css/main.e52a35f7.chunk.css"
+  },
+  {
+    "revision": "1b4972fc244bcf7372d2",
+    "url": "/crwn-clothing/static/js/2.36e3efa4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/crwn-clothing/static/js/2.36e3efa4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba7c54e9399d5c233d37",
+    "url": "/crwn-clothing/static/js/main.1301f23f.chunk.js"
+  },
+  {
+    "revision": "0c0bf0a54c9e5596672f",
+    "url": "/crwn-clothing/static/js/runtime-main.5ffcf0f1.js"
+  }
+]);
